ff96be87986ceafd3a44016966cd21436cb34460
Andrew_Chen <72327157+Andrew3927@users.noreply.github.com>
2022-09-20 18:50:17 +0800

    new empty project

---
+++ lect03_assignment1_dail_app/app/src/main/java/com/example/lect03_assignment1_dail_app/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.lect03_assignment1_dail_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
