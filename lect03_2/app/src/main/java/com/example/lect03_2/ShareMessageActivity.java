b4a3a9ec3be62f8807acaa357fd566b845b6512c
Andrew_Chen <72327157+Andrew3927@users.noreply.github.com>
2022-09-20 17:17:51 +0800

    refactor the Java Class's name

--- lect03_2/app/src/main/java/com/example/lect03_2/ShareMessageActivity.java
+++ lect03_2/app/src/main/java/com/example/lect03_2/ShareMessageActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class ShareMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
