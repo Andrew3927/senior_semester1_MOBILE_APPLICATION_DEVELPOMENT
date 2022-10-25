550b9fb11f2b5c4823b4a3e9ff00c752d9dca731
Andrew_Chen <72327157+Andrew3927@users.noreply.github.com>
2022-10-25 11:41:14 +0800

    lecture 07 fragment (strong inner communication)

--- lect07_1_workout/app/src/main/java/com/example/workout/DetailActivity.java
+++ lect07_1_workout/app/src/main/java/com/example/workout/DetailActivity.java
@@ -5,10 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
+    public static final String EXTRA_WORKOUT_ID = "id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        WorkoutDetailFragment frag = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_workout);
+//        frag.setWorkoutId(1);
+        int workoutID = (int) getIntent().getExtras().get(EXTRA_WORKOUT_ID);
+        frag.setWorkoutId(workoutID);
     }
+
 }
